7962ae3922f61e42911a3591e117ac12f749b5c2
Francesco C <fran461j@outlook.it>
2023-03-20 17:50:14 +0100

    class array

--- Program.cs
+++ Program.cs
@@ -1,35 +1,11 @@
-﻿//prodotto è caratterizzato da:
-//-codice(numero intero)
-//- nome
-//- descrizione
-//- prezzo
-//- iva
-//Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
-//alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
-//Il codice prodotto sia accessibile solo in lettura
-//Gli altri attributi siano accessibili sia in lettura che in scrittura
-//Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
-//Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
-//Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interargirci per testare tutti i metodi che avete previsto.
+﻿Product[] productList = new Product[3];
+productList[0] = new Product("Pasta", "500gr grano italiano", 1.50, 22);
+productList[1] = new Product("Tonno", "200gr Tonno del mar Ionio", 2.00, 22);
+productList[2] = new Product("Pane", "1kg pane duro", 1.00, 22);
 
-
-Console.Write("Inserisci nome: ");
-string name =Console.ReadLine() ?? "";
-
-Console.Write("Inserisci descrizione: ");
-string description = Console.ReadLine() ?? "";
-
-Console.Write("Inserisci prezzo: ");
-double price = Convert.ToDouble(Console.ReadLine());
-
-Console.Write("Inserisci iva: ");
-double iva = Convert.ToDouble(Console.ReadLine());
-
-Product pasta = new Product(name, description, price, iva);
-
-
-Console.WriteLine(pasta.ToString());
-
-Console.WriteLine($"Prezzo iva compreso: {pasta.GetFullprice()}$");
-
-Console.WriteLine($"Codice seriale: {pasta.GetCode()}");
+foreach (Product product in productList)
+{
+    Console.WriteLine(product.ToString());
+    Console.WriteLine($"prezzo iva compresa: {product.GetFullprice()}");
+    Console.WriteLine($"codice con pad {product.GetCode()}");
+}
