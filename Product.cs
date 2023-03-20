1fc1144e034363f45c082e646dd86fd2f6a3d97a
Francesco C <fran461j@outlook.it>
2023-03-20 15:03:30 +0100

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Product
+{
+    private int code;
+    private string name;
+    private string description;
+    private double price;
+    private double iva;
+
+    public Product(string name, string description, double price, double iva)
+    {
+        Random rnd = new Random();
+        this.code = rnd.Next(1, 99999999);
+
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.iva = iva;
+    }
+
+
+
+    //properties
+    public int Code
+    {
+        get { return code; }
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    public string Description
+    {
+        get { return description; }
+        set { description = value; }
+    }
+
+    public double Price
+    {
+        get { return price; }
+        set { price = value; }
+    }
+
+    public double Iva
+    {
+        get { return iva; }
+        set { iva = value; }
+    }
+
+    public override string ToString()
+    {
+        var bk = Environment.NewLine;
+        return 
+            $"{bk}" +
+            $"Nome prodotto: {name} {bk}" +
+            $"Descrizione prodotto: {description} {bk}" +
+            $"Prezzo prodotto: {price}€ {bk}" +
+            $"Iva prodotto: {iva}% {bk}" +
+            $"Codice prodotto: {code} {bk}";
+    }
+
+};
+
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//prodotto è caratterizzato da:
+//-codice(numero intero)
+//- nome
+//- descrizione
+//- prezzo
+//- iva
+//Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+//alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+//Il codice prodotto sia accessibile solo in lettura
+//Gli altri attributi siano accessibili sia in lettura che in scrittura
+//Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+//Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+//Testate poi i vostri oggetti Prodotto, istanziandoli e provando ad interargirci per testare tutti i metodi che avete previsto.
+
+
+Console.Write("Inserisci nome: ");
+string name =Console.ReadLine() ?? "";
+
+Console.Write("Inserisci descrizione: ");
+string description = Console.ReadLine() ?? "";
+
+Console.Write("Inserisci prezzo: ");
+double price = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Inserisci iva: ");
+double iva = Convert.ToDouble(Console.ReadLine());
+
+Product pasta = new Product(name, description, price, iva);
+
+
+Console.WriteLine(pasta.ToString());
